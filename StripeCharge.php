d2e6587d8090ad8728990cc8867a3e7e22a60d76
grmankar <ganesh.alkurn@gmail.com>
2017-05-05 18:49:29 +0530

    Add files via upload
    update

---
+++ StripeCharge.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @copyright Copyright Ganesh Alkurn, 2017
+ * @license https://github.com/alkurn/yii2-stripe/LICENSE
+ * @link https://github.com/alkurn/yii2-stripe#readme
+ */
+
+namespace alkurn\stripe;
+
+use Yii;
+use Stripe\Stripe;
+use Stripe\Charge;
+
+/**
+ * Yii stripe component.
+ *
+ * @author Ganesh Alkurn <ganesh.alkurn@gmail.com>
+ */
+class StripeCharge extends Stripe {
+
+
+    public function createCharge($request){
+
+        Stripe::setApiKey(Yii::$app->stripe->privateKey);
+        return Charge::create($request);
+    }
+
+}
+
