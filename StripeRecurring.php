dcb38508317e93cc8f00bca2e2acd97157bec6a6
Alkurn Tech <tech.alkurn@gmail.com>
2019-01-08 14:44:02 +0530

    Update

--- StripeRecurring.php
+++ StripeRecurring.php
@@ -13,33 +13,52 @@ use Stripe\Stripe;
 use Stripe\Charge;
 use Stripe\Customer;
 use Stripe\Subscription;
-    /**
+
+/**
  * Yii stripe component.
  *
  * @author Ganesh Alkurn <ganesh.alkurn@gmail.com>
  */
-class StripeRecurring extends Stripe {
+class StripeRecurring extends Stripe
+{
 
 
-    public function createCharge($request){
+    public function createCharge($request)
+    {
 
         Stripe::setApiKey(Yii::$app->stripe->privateKey);
-        $customer = Customer::create(['source'  => $request['token'], 'email' => $request['email']]);
-        return Subscription::create([ 'customer' => $customer->id, 'items'  => $request['items'] ]);
+        $customer = Customer::create(['source' => $request['token'], 'email' => $request['email']]);
+        return Subscription::create(['customer' => $customer->id, 'items' => $request['items']]);
 
 
     }
 
-    public function cancelSubscription($subscription_id){
+    public function cancelSubscription($subscription_id)
+    {
 
         Stripe::setApiKey(Yii::$app->stripe->privateKey);
         $sub = Subscription::retrieve($subscription_id);
         return ($sub) ? $sub->cancel() : false;
-
-
     }
 
-    
+    public function switchSubscription($subscription_id, $plan)
+    {
+        if(!$plan) return false;
+        Stripe::setApiKey(Yii::$app->stripe->privateKey);
+        $sub = Subscription::retrieve($subscription_id);
+        $res = Subscription::update($subscription_id, [
+            'cancel_at_period_end' => false,
+            'items' => [
+                [
+                    'id' => $subscription->items->data[0]->id,
+                    'plan' => $plan,
+                ],
+            ],
+        ]);
+
+        return ($res) ? true : false;
+    }
+
 
 }
 
